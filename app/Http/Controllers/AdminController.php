52c60b23f3e5d51ef8f49eac1266208fdce25d46
minhvu278 <dominhvu278@gmail.com>
2022-11-20 19:16:12 +0700

    Admin profile & image update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,5 +18,37 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
+    }
+
+    public function profile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.profile', compact('adminData'));
+    }
+
+    public function editProfile() {
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.edit_profile', compact('editData'));
+    }
+
+    public function storeProfile(Request $request) {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+
+        if ($request->file('profile_image')) {
+            $file = $request->file('profile_image');
+
+            $fileName = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $fileName);
+            $data['profile_image'] = $fileName;
+        }
+        $data->save();
+
+        return redirect()->route('admin.profile');
     }
 }
