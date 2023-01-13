c8176d03d202d1b9dcf0788ea79b7275ef1f709a
minhvu278 <dominhvu278@gmail.com>
2023-01-13 17:38:34 +0700

    Upload slide

--- app/Models/HomeSlide.php
+++ app/Models/HomeSlide.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HomeSlider extends Model
+class HomeSlide extends Model
 {
     use HasFactory;
 
