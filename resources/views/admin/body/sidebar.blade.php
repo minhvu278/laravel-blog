8ede9b21a72145041c0f9ea31f08165b06f837ab
minhvu278 <dominhvu278@gmail.com>
2023-01-27 22:17:32 +0700

    Backend about page

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -29,13 +29,17 @@
                         <span>Home Slide Setup</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow waves-effect">
-                                <i class="ri-mail-send-line"></i>
-                                <span>Home Slide Setup</span>
-                            </a>
-                                <li><a href="{{ route('home.slide') }}">Home Slide</a></li>
-                        </li>
+                        <li><a href="{{ route('home.slide') }}">Home Slide</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-mail-send-line"></i>
+                        <span>About Page</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="{{ route('about.page') }}">About</a></li>
                     </ul>
                 </li>
 
